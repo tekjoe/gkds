5fa6fc5e9b6d798bafb85183a149836b88e8a6d9
tekjoe <ramirez.josephh@gmail.com>
2020-11-24 07:28:19 -0600

    added google analytics script

--- header.php
+++ header.php
@@ -2,6 +2,19 @@
 <html lang="en" id="html">
 
 <head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127178111-7"></script>
+  <script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'UA-127178111-7');
+  </script>
+
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title><?php wp_title('|', true, 'right'); ?> Gustavo Krystal Dance Studio</title>
